e85f201fe4793608f678abdb216bacaa3e311a1c
ALEXIS683 <alexis.cordier683@gmail.com>
2023-02-11 19:12:34 +0100

    refactor: from server.js to requests.js

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,3 +1,23 @@
+import util from 'util';
+import { pipeline } from "stream";
+import fs from 'fs';
+
+const pump = util.promisify(pipeline);
+
 export const index = (req, res) => {
     res.view('templates/index.ejs');
+}
+
+export const upload = async (req, res) => {
+    const parts = req.parts();
+    for await (const part of parts) {
+        if (part.file) {
+            await pump(part.file, fs.createWriteStream(`./input_nifi/${part.filename}`));
+        } else {
+            console.log('got field: ', part);
+        }
+    }
+
+    console.log('Upload completed !');
+    res.redirect('/');
 }
--- src/server.js
+++ src/server.js
@@ -2,15 +2,11 @@ import Fastify from "fastify";
 import fastifyView from "@fastify/view";
 import fastifyMultipart from "@fastify/multipart";
 import ejs from 'ejs';
-import fs from 'fs';
-import util from 'util';
-import { pipeline } from "stream";
-import { index } from "./actions/requests.js";
+import { index, upload } from "./actions/requests.js";
 
 const PORT = 8083 || process.env.PORT;
 
 const app = Fastify();
-const pump = util.promisify(pipeline);
 
 app.register(fastifyMultipart)
 
@@ -18,33 +14,21 @@ app.register(fastifyView, {
     engine:{
         ejs
     }
-})
-
+});
 
 app.get('/', index);
 
-app.post('/upload', async (req, res) => {
-    const parts = req.parts();
-    for await (const part of parts) {
-        if (part.file) {
-            await pump(part.file, fs.createWriteStream(`./input_nifi/${part.filename}`));
-        } else {
-            console.log('got field: ', part);
-        }
-    }
-
-    console.log('upload completed');
-});
+app.post('/upload', upload);
 
 
 const start = async () => {
     try {
         await app.listen({port:PORT})
         console.log(`Server running on port ${PORT}`);
-    } catch (err){
+    } catch (err) {
         console.error(err);
         process.exit(1);
     }
-}
+};
 
-start()
+start();
